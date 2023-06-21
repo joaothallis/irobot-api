b2e10d4611c77e26cf200772af00d8e04c0a3b6c
João Thallis <joaothallis@icloud.com>
2023-06-21 13:10:03 -0300

    Format

--- examples/navigation.py
+++ examples/navigation.py
@@ -8,9 +8,10 @@ from irobot_edu_sdk.backend.bluetooth import Bluetooth
 from irobot_edu_sdk.robots import event, hand_over, Color, Robot, Root, Create3
 from irobot_edu_sdk.music import Note
 
-robot = Create3(Bluetooth('iRobot-06C2ACFB22E5481198A7D9'))
+robot = Create3(Bluetooth("iRobot-06C2ACFB22E5481198A7D9"))
 speed = 10
 th = 150
+
 
 async def forward(robot):
     await robot.set_lights_rgb(0, 255, 0)
@@ -22,12 +23,13 @@ async def backoff(robot):
     await robot.move(-20)
     await robot.turn_left(45)
 
+
 @event(robot.when_bumped, [True, True])
 async def bumped(robot):
     await robot.move(-10)
     await robot.turn_right(45)
-    await forward(robot) 
-    while enabled():            
+    await forward(robot)
+    while enabled():
         sensors = (await robot.get_ir_proximity()).sensors
         if front_obstacle(sensors):
             await backoff(robot)
@@ -38,13 +40,14 @@ async def bumped(robot):
         if right_obstacle(sensors):
             await backoff(robot)
             await forward(robot)
+
 
 @event(robot.when_bumped, [True, False])
 async def bumped(robot):
     await robot.move(-10)
     await robot.turn_right(45)
-    await forward(robot) 
-    while enabled():            
+    await forward(robot)
+    while enabled():
         sensors = (await robot.get_ir_proximity()).sensors
         if front_obstacle(sensors):
             await backoff(robot)
@@ -55,13 +58,14 @@ async def bumped(robot):
         if right_obstacle(sensors):
             await backoff(robot)
             await forward(robot)
+
 
 @event(robot.when_bumped, [False, True])
 async def bumped(robot):
     await robot.move(-10)
     await robot.turn_right(45)
-    await forward(robot) 
-    while enabled():            
+    await forward(robot)
+    while enabled():
         sensors = (await robot.get_ir_proximity()).sensors
         if front_obstacle(sensors):
             await backoff(robot)
@@ -72,20 +76,24 @@ async def bumped(robot):
         if right_obstacle(sensors):
             await backoff(robot)
             await forward(robot)
+
 
 def front_obstacle(sensors):
     return sensors[3] > th
 
+
 def left_obstacle(sensors):
     return sensors[2] > th
+
 
 def right_obstacle(sensors):
     return sensors[4] > th
 
+
 @event(robot.when_play)
 async def play(robot):
-    await forward(robot) 
-    while enabled():            
+    await forward(robot)
+    while enabled():
         sensors = (await robot.get_ir_proximity()).sensors
         if front_obstacle(sensors):
             await backoff(robot)
@@ -97,12 +105,14 @@ async def play(robot):
             await backoff(robot)
             await forward(robot)
 
+
 def enabled():
     f = open("../run", "r")
     run = f.read()
-    if 'true' in run:
+    if "true" in run:
         return True
     else:
         exit()
+
 
 robot.play()
